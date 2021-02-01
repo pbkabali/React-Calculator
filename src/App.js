02b80a9d28cf2de550b9ea104f9359416ee310c3
Paul Balitema Kabali <engineerbpk@gmail.com>
2021-02-02 00:23:19 +0300

    Delete unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Welcome!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Welcome to React Calculator!</p>
       </header>
     </div>
   );
