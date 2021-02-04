8d4b15ecfe446e27bdfbd5b07bc864b9f945980d
Paul Balitema Kabali <engineerbpk@gmail.com>
2021-02-04 20:36:35 +0300

    Transform App into a class component and initialize state

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,26 @@
+import { Component } from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
+import calculate from '../logic/calculate';
 import '../App.css';
 
-const App = () => (
-  <>
-    <Display result="12345" />
-    <ButtonPanel />
-  </>
-);
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
 
+  render() {
+    return (
+      <>
+        <Display result="12345" />
+        <ButtonPanel />
+      </>
+    );
+  }
+}
 export default App;
