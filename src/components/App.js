2e31ffc56361291998bc2c735691b17327eb9fc5
Paul Balitema Kabali <engineerbpk@gmail.com>
2021-02-09 01:53:57 +0300

    Modify App component to use hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,47 +1,40 @@
 /* eslint-disable react/no-unused-state */
 
-import { Component } from 'react';
+import { useState } from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import calculate from '../logic/calculate';
 import '../App.css';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-    };
+const App = () => {
+  const [status, setStatus] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
 
-    this.handleClick = this.handleClick.bind(this);
+  const handleClick = buttonName => {
+    setStatus(prevState => calculate(prevState, buttonName));
+  };
+
+  const { total, next } = status;
+
+  let result;
+  if (total === null && next === null) {
+    result = '0';
+  } else if (next === null) {
+    result = total;
+  } else {
+    result = next;
   }
 
-  handleClick(buttonName) {
-    this.setState(prevState => calculate(prevState, buttonName));
-  }
-
-  render() {
-    const { total, next } = this.state;
-
-    let result;
-    if (total === null && next === null) {
-      result = '0';
-    } else if (next === null) {
-      result = total;
-    } else {
-      result = next;
-    }
-
-    return (
-      <>
-        <Display result={result} />
-        <ButtonPanel clickHandler={this.handleClick} />
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <Display result={result} />
+      <ButtonPanel clickHandler={handleClick} />
+    </>
+  );
+};
 export default App;
 
 /* eslint-enable react/no-unused-state */
