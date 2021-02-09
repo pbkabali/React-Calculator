7f356399df8417797c0bf57035a06a37f094d2b1
Paul Balitema Kabali <engineerbpk@gmail.com>
2021-02-09 21:00:19 +0300

    Add style to the quote page

--- src/components/QuotePage.js
+++ src/components/QuotePage.js
@@ -1,3 +1,9 @@
-const QuotePage = () => <p>This is a Joke</p>;
-
+const QuotePage = () => (
+  <div className="side-padding home-container">
+    <p>
+      What did the triangle say to the circle?
+      <strong> “You’re pointless.”</strong>
+    </p>
+  </div>
+);
 export default QuotePage;
