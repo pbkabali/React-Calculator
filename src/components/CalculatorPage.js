c6c22209e25ab320f2e69b65956bec6b7d7f80ed
Paul Balitema Kabali <engineerbpk@gmail.com>
2021-02-09 20:35:25 +0300

    Add style to the calculator page

--- src/components/CalculatorPage.js
+++ src/components/CalculatorPage.js
@@ -1,5 +1,12 @@
 import Calculator from './Calculator';
 
-const CalculatorPage = () => <Calculator />;
+const CalculatorPage = () => (
+  <div className="flex side-padding calc-container">
+    <h2>Let&apos;s do some math!</h2>
+    <div>
+      <Calculator />
+    </div>
+  </div>
+);
 
 export default CalculatorPage;
